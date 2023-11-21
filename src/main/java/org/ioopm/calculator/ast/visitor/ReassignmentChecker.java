bb48abbff60b5f4f9f2299992dc13473ece8dcd7
Casper <casper.bjorkman@gmail.com>
2023-11-21 11:54:38 +0100

    Implemented NamedConstantChecker and ReassignmentChecker

---
+++ src/main/java/org/ioopm/calculator/ast/visitor/ReassignmentChecker.java
@@ -0,0 +1,27 @@
+package org.ioopm.calculator.ast.visitor;
+
+import org.ioopm.calculator.ast.Assignment;
+import org.ioopm.calculator.ast.Variable;
+
+import java.util.HashSet;
+
+public class ReassignmentChecker extends AstChecker {
+    final HashSet<Variable> boundVariables = new HashSet<>();
+    final HashSet<Variable> reassignedVariables = new HashSet<>();
+
+    @Override
+    public Boolean visit(Assignment n) {
+        super.visit(n);
+        Variable assignmentTarget = (Variable) n.getRhs();
+
+        if (boundVariables.contains(assignmentTarget)) {
+            reassignedVariables.add(assignmentTarget);
+            return false;
+        }
+
+        boundVariables.add(assignmentTarget);
+        return true;
+
+
+    }
+}
